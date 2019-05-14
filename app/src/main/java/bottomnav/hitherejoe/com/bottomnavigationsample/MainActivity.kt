ac534d9743475a543e70d4b06332793ba4fd86ad
jerry <309032663@qq.com>
2019-05-14 19:50:54 +0800

    update

--- app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/MainActivity.kt
+++ app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/MainActivity.kt
@@ -1,36 +1,73 @@
 package bottomnav.hitherejoe.com.bottomnavigationsample
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    private val favoritesTabIndex = 0
+    private val schedulesTabIndex = 1
+    private val mineTabIndex = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fragments = getFragments()
+
+        vp.adapter = object : FragmentStateAdapter(this) {
+            override fun getItem(position: Int): Fragment {
+                return fragments[position]
+            }
+
+            override fun getItemCount(): Int {
+                return fragments.size
+            }
+        }
+        //禁用左右滑动切换页签
+        vp.isUserInputEnabled= false
+
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.action_favorites -> {
-                    textFavorites.visibility = View.VISIBLE
-                    textSchedules.visibility = View.GONE
-                    textMusic.visibility = View.GONE
+                    vp.setCurrentItem(favoritesTabIndex,false)
                 }
                 R.id.action_schedules -> {
-                    textFavorites.visibility = View.GONE
-                    textSchedules.visibility = View.VISIBLE
-                    textMusic.visibility = View.GONE
+                    vp.setCurrentItem(schedulesTabIndex,false)
                 }
                 R.id.action_music -> {
-                    textFavorites.visibility = View.GONE
-                    textSchedules.visibility = View.GONE
-                    textMusic!!.visibility = View.VISIBLE
+                    vp.setCurrentItem(mineTabIndex,false)
                 }
             }
-            false
+            true
         }
+    }
+
+    private fun getFragments(): ArrayList<Fragment> {
+        val fragments = ArrayList<Fragment>(3)
+
+        val favoritesFragment = BaseFragment()
+        var bundle = Bundle()
+        bundle.putString("title", "favorites")
+        favoritesFragment.arguments = bundle
+
+        val schedulesFragment = BaseFragment()
+        bundle = Bundle()
+        bundle.putString("title", "schedules")
+        schedulesFragment.arguments = bundle
+
+        val mineFragment = BaseFragment()
+        bundle = Bundle()
+        bundle.putString("title", "mine")
+        mineFragment.arguments = bundle
+
+        fragments.add(favoritesFragment)
+        fragments.add(schedulesFragment)
+        fragments.add(mineFragment)
+        return fragments
     }
 }
