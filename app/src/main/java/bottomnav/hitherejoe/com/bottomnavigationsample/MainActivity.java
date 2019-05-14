87a923d4eb8d52abdf3fe2f3a702f5afc37fd19c
jerry <309032663@qq.com>
2019-05-14 19:16:24 +0800

    update

--- app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/MainActivity.java
+++ app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/MainActivity.java
@@ -1,8 +1,8 @@
 package bottomnav.hitherejoe.com.bottomnavigationsample;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
